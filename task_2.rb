d243ac95b933b5a60dfab998a011602a239df03c
Artem Dordovsky <DordovskyArtem@gmail.com>
2013-06-05 11:38:21 +0300

    the solution of the problem 2

---
+++ task_2.rb
@@ -0,0 +1,29 @@
+class Euler
+  def initialize(n=4000000)
+    @n = n
+    @fibs_array = []
+  end
+
+  def solve
+    (1...@n).map do |i| 
+      if fib(i)<@n
+        @fibs_array.push(fib(i))
+      else
+        break
+      end
+    end
+    sum = @fibs_array.inject(0){|result, i| i.even?? result+i : result}
+  end
+
+  private
+  def fib(n, a=0, b=1)
+    if n==0
+      a
+    else
+      fib(n-1, b, a+b)
+    end
+  end
+end
+
+task_2 = Euler.new
+p task_2.solve
